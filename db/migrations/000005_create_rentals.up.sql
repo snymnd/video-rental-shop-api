eb6029371702aa1b20b07f4c31cf9078d9ff0a92
Muhammad Yunus <muhammad.yunus@shopee.com>
2025-06-02 23:03:57 +0700

    feat: add rental and latefee payment

--- db/migrations/000005_create_rentals.up.sql
+++ db/migrations/000005_create_rentals.up.sql
@@ -1,12 +1,30 @@
+BEGIN;
+
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'rental_status') THEN
+    -- pending is status for rented videos with payment status of pending
+    -- failed is status for rented videos with payment status of expired
+    -- rented is status for rented videos with payment status of success
+        CREATE TYPE rental_status AS ENUM ('rented', 'returned', 'pending', 'failed');
+    END IF;
+END $$;
+
+
 CREATE TABLE  IF NOT EXISTS rentals (
   id BIGSERIAL PRIMARY KEY,
   video_id BIGINT NOT NULL,
-  payment_id BIGINT NOT NULL,
+  rental_payment_id BIGINT,
+  latefee_payment_id BIGINT,
   due_date TIMESTAMP NOT NULL,
   return_date TIMESTAMP,
+  status rental_status NOT NULL DEFAULT 'pending',
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
   deleted_at TIMESTAMP,
   FOREIGN KEY (video_id) REFERENCES videos(id),
-  FOREIGN KEY (payment_id) REFERENCES payments(id)
+  FOREIGN KEY (rental_payment_id) REFERENCES payments(id),
+  FOREIGN KEY (latefee_payment_id) REFERENCES payments(id)
 );
+
+COMMIT;
